f715b84c15ad622a6e387a6dd3ac338e0691e5a7
Kacper Kogut <kogutkacper7@gmail.com>
2025-09-16 18:32:10 +0200

    Solution4

--- app/main.py
+++ app/main.py
@@ -7,7 +7,11 @@ class Cargo:
 
 
 class BaseRobot:
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int, coords: list | None = None
+    ) -> None:
         if coords is None:
             coords = [0, 0]
         self.name = name
@@ -31,10 +35,22 @@ class BaseRobot:
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list | None = None
+    ) -> None:
         if coords is None:
-            coords = [0, 0, 0]
-        super().__init__(name=name, weight=weight, coords=coords)
+            coords3 = [0, 0, 0]
+        elif len(coords) == 2:
+            coords3 = coords + [0]
+        elif len(coords) == 3:
+            coords3 = coords
+        else:
+            raise ValueError
+
+        super().__init__(name=name, weight=weight, coords=coords3)
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -47,8 +63,8 @@ class DeliveryDrone(FlyingRobot):
     def __init__(
             self, name: str,
             weight: int,
-            coords: list = None,
-            max_load_weight: int = None,
+            coords: list | None = None,
+            max_load_weight: int | None = None,
             current_load: Cargo | None = None
     ) -> None:
         super().__init__(name=name, weight=weight, coords=coords)
